33a1f2e1b1df49eb36421842cd84f4b862ed1551
Arpan Kumar <arpan.kumar@microchip.com>
2022-03-02 11:33:59 +0530

    [ADCHS] added interrupt demo
    MH3-63246

---
+++ apps/adchs/adchs_interrupt/firmware/src/packs/WBZ351_DFP/instance/evsys.h
@@ -0,0 +1,54 @@
+/*
+ * Instance header file for WBZ351
+ *
+ * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
+ *
+ * Subject to your compliance with these terms, you may use Microchip software and any derivatives
+ * exclusively with Microchip products. It is your responsibility to comply with third party license
+ * terms applicable to your use of third party software (including open source software) that may
+ * accompany Microchip software.
+ *
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY,
+ * APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL
+ * LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF
+ * MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE FULLEST EXTENT
+ * ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT
+ * EXCEED THE AMOUNT OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ */
+
+/* file generated from device description version 2022-02-03T13:54:44Z */
+#ifndef _WBZ35_EVSYS_INSTANCE_
+#define _WBZ35_EVSYS_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for EVSYS peripheral ========== */
+#define EVSYS_INSTANCE_ID                        _UL_(35)   
+#define EVSYS_CHIP_EVSYS_PADDR_MSB               _UL_(12)   
+#define EVSYS_CHIP_EVSYS_EXTEVT_NUM              _UL_(0)    
+#define EVSYS_CHIP_EVSYS_ASYNCHRONOUS_CHANNELS   _UL_(0xFFFFF000) 
+#define EVSYS_CHIP_EVSYS_CHANNELS                _UL_(32)   
+#define EVSYS_CHIP_EVSYS_GENERATORS              _UL_(81)   
+#define EVSYS_CHIP_EVSYS_SYNCH_NUM               _UL_(12)   
+#define EVSYS_CHIP_EVSYS_USERS                   _UL_(57)   
+#define EVSYS_CHIP_EVSYS_CHANNELS_BITS           _UL_(5)    
+#define EVSYS_CHIP_EVSYS_GENERATORS_BITS         _UL_(7)    
+#define EVSYS_CHIP_EVSYS_SYNCH_NUM_BITS          _UL_(4)    
+#define EVSYS_CHIP_EVSYS_USERS_BITS              _UL_(6)    
+#define EVSYS_GCLK_ID_CH0                        _UL_(8)    
+#define EVSYS_GCLK_ID_CH1                        _UL_(9)    
+#define EVSYS_GCLK_ID_CH2                        _UL_(10)   
+#define EVSYS_GCLK_ID_CH3                        _UL_(11)   
+#define EVSYS_GCLK_ID_CH4                        _UL_(12)   
+#define EVSYS_GCLK_ID_CH5                        _UL_(13)   
+#define EVSYS_GCLK_ID_CH6                        _UL_(14)   
+#define EVSYS_GCLK_ID_CH7                        _UL_(15)   
+#define EVSYS_GCLK_ID_CH8                        _UL_(16)   
+#define EVSYS_GCLK_ID_CH9                        _UL_(17)   
+#define EVSYS_GCLK_ID_CH10                       _UL_(18)   
+#define EVSYS_GCLK_ID_CH11                       _UL_(19)   
+
+#endif /* _WBZ35_EVSYS_INSTANCE_ */
---
+++ apps/adchs/adchs_interrupt/readme.md
@@ -0,0 +1,52 @@
+[![MCHP](https://www.microchip.com/ResourcePackages/Microchip/assets/dist/images/logo.png)](https://www.microchip.com)
+
+# ADCHS Interrupt
+
+This example application shows how to sample an analog input using the ADCHS peripheral and displays the converted samples on a serial terminal.
+
+## Description
+
+In this application, an analog input is converted by a software edge trigger. Converted digital value is displayed on the serial terminal.
+
+## Downloading and building the application
+
+To download or clone this application from Github, go to the [top level of the repository](https://github.com/Microchip-MPLAB-Harmony/csp_apps_pic32cx_bz24_bz25) and click
+
+![clone](../../../docs/images/clone.png)
+
+Path of the application within the repository is **apps/adchs/adchs_interrupt/firmware** .
+
+To build the application, refer to the following table and open the project using its IDE.
+
+| Project Name      | Description                                    |
+| ----------------- | ---------------------------------------------- |
+| WBZ351_curiosity.X | MPLABX project for [PIC32CX WBZ351 Curiosity Board]() |
+|||
+
+## Setting up the hardware
+
+The following table shows the target hardware for the application projects.
+
+| Project Name| Board|
+|:---------|:---------:|
+| WBZ351_curiosity.X | MPLABX project for [PIC32CX WBZ351 Curiosity Board]() |
+|||
+
+### Setting up [PIC32CX WBZ351 Curiosity Board]()
+
+- Connect the AN5 pin RB1 (Pin 8 of XPRO Header) to the Vcc (Pin 20 of XPRO Header)
+- Connect the Debug USB port on the board to the computer using a micro USB cable
+
+## Running the Application
+
+1. Open the Terminal application (Ex.:Tera term) on the computer
+2. Connect to the "USB to UART" COM port and configure the serial settings as follows:
+    - Baud : 115200
+    - Data : 8 Bits
+    - Parity : None
+    - Stop : 1 Bit
+    - Flow Control : None
+3. Build and Program the application project using its IDE
+4. Console displays the ADC count and the ADC input voltage
+
+![output](images/output_adchs_interrupt.png)
