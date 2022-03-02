68e8de58bdd7a0695409dc893bd656af350d939f
Arpan Kumar <arpan.kumar@microchip.com>
2022-03-02 11:50:24 +0530

    [SPI] added master demo
    MH3-63263

---
+++ apps/sercom/spi/spi_eeprom_write_read/firmware/src/config/wbz351_curiosity/peripheral/gpio/plib_gpio.h
@@ -0,0 +1,263 @@
+/*******************************************************************************
+  GPIO PLIB
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_gpio.h
+
+  Summary:
+    GPIO PLIB Header File
+
+  Description:
+    This library provides an interface to control and interact with Parallel
+    Input/Output controller (GPIO) module.
+
+*******************************************************************************/
+
+/*******************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+
+#ifndef PLIB_GPIO_H
+#define PLIB_GPIO_H
+
+#include <device.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+    extern "C" {
+
+#endif
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Data types and constants
+// *****************************************************************************
+// *****************************************************************************
+
+/*** Macros for LED pin ***/
+#define LED_Set()               (GPIOB_REGS->GPIO_LATSET = (1<<3))
+#define LED_Clear()             (GPIOB_REGS->GPIO_LATCLR = (1<<3))
+#define LED_Toggle()            (GPIOB_REGS->GPIO_LATINV= (1<<3))
+#define LED_Get()               ((GPIOB_REGS->GPIO_PORT >> 3) & 0x1)
+#define LED_OutputEnable()      (GPIOB_REGS->GPIO_TRISCLR = (1<<3))
+#define LED_InputEnable()       (GPIOB_REGS->GPIO_TRISSET = (1<<3))
+#define LED_PIN                  GPIO_PIN_RB3
+/*** Macros for EEPROM_CS pin ***/
+#define EEPROM_CS_Set()               (GPIOA_REGS->GPIO_LATSET = (1<<9))
+#define EEPROM_CS_Clear()             (GPIOA_REGS->GPIO_LATCLR = (1<<9))
+#define EEPROM_CS_Toggle()            (GPIOA_REGS->GPIO_LATINV= (1<<9))
+#define EEPROM_CS_Get()               ((GPIOA_REGS->GPIO_PORT >> 9) & 0x1)
+#define EEPROM_CS_OutputEnable()      (GPIOA_REGS->GPIO_TRISCLR = (1<<9))
+#define EEPROM_CS_InputEnable()       (GPIOA_REGS->GPIO_TRISSET = (1<<9))
+#define EEPROM_CS_PIN                  GPIO_PIN_RA9
+/*** Macros for EEPROM_HOLD pin ***/
+#define EEPROM_HOLD_Set()               (GPIOB_REGS->GPIO_LATSET = (1<<10))
+#define EEPROM_HOLD_Clear()             (GPIOB_REGS->GPIO_LATCLR = (1<<10))
+#define EEPROM_HOLD_Toggle()            (GPIOB_REGS->GPIO_LATINV= (1<<10))
+#define EEPROM_HOLD_Get()               ((GPIOB_REGS->GPIO_PORT >> 10) & 0x1)
+#define EEPROM_HOLD_OutputEnable()      (GPIOB_REGS->GPIO_TRISCLR = (1<<10))
+#define EEPROM_HOLD_InputEnable()       (GPIOB_REGS->GPIO_TRISSET = (1<<10))
+#define EEPROM_HOLD_PIN                  GPIO_PIN_RB10
+/*** Macros for EEPROM_WP pin ***/
+#define EEPROM_WP_Set()               (GPIOB_REGS->GPIO_LATSET = (1<<2))
+#define EEPROM_WP_Clear()             (GPIOB_REGS->GPIO_LATCLR = (1<<2))
+#define EEPROM_WP_Toggle()            (GPIOB_REGS->GPIO_LATINV= (1<<2))
+#define EEPROM_WP_Get()               ((GPIOB_REGS->GPIO_PORT >> 2) & 0x1)
+#define EEPROM_WP_OutputEnable()      (GPIOB_REGS->GPIO_TRISCLR = (1<<2))
+#define EEPROM_WP_InputEnable()       (GPIOB_REGS->GPIO_TRISSET = (1<<2))
+#define EEPROM_WP_PIN                  GPIO_PIN_RB2
+
+
+// *****************************************************************************
+/* GPIO Port
+
+  Summary:
+    Identifies the available GPIO Ports.
+
+  Description:
+    This enumeration identifies the available GPIO Ports.
+
+  Remarks:
+    The caller should not rely on the specific numbers assigned to any of
+    these values as they may change from one processor to the next.
+
+    Not all ports are available on all devices.  Refer to the specific
+    device data sheet to determine which ports are supported.
+*/
+
+typedef enum
+{
+    GPIO_PORT_A = GPIOA_BASE_ADDRESS,
+    GPIO_PORT_B = GPIOB_BASE_ADDRESS,
+} GPIO_PORT;
+
+typedef enum
+{
+    GPIO_INTERRUPT_ON_MISMATCH,
+    GPIO_INTERRUPT_ON_RISING_EDGE,
+    GPIO_INTERRUPT_ON_FALLING_EDGE,
+    GPIO_INTERRUPT_ON_BOTH_EDGES,
+}GPIO_INTERRUPT_STYLE;
+
+// *****************************************************************************
+/* GPIO Port Pins
+
+  Summary:
+    Identifies the available GPIO port pins.
+
+  Description:
+    This enumeration identifies the available GPIO port pins.
+
+  Remarks:
+    The caller should not rely on the specific numbers assigned to any of
+    these values as they may change from one processor to the next.
+
+    Not all pins are available on all devices.  Refer to the specific
+    device data sheet to determine which pins are supported.
+*/
+
+typedef enum
+{
+    GPIO_PIN_RA0 = 0,
+    GPIO_PIN_RA1 = 1,
+    GPIO_PIN_RA2 = 2,
+    GPIO_PIN_RA3 = 3,
+    GPIO_PIN_RA4 = 4,
+    GPIO_PIN_RA5 = 5,
+    GPIO_PIN_RA6 = 6,
+    GPIO_PIN_RA7 = 7,
+    GPIO_PIN_RA8 = 8,
+    GPIO_PIN_RA9 = 9,
+    GPIO_PIN_RA10 = 10,
+    GPIO_PIN_RA11 = 11,
+    GPIO_PIN_RA12 = 12,
+    GPIO_PIN_RA13 = 13,
+    GPIO_PIN_RA14 = 14,
+    GPIO_PIN_RB0 = 16,
+    GPIO_PIN_RB1 = 17,
+    GPIO_PIN_RB2 = 18,
+    GPIO_PIN_RB3 = 19,
+    GPIO_PIN_RB4 = 20,
+    GPIO_PIN_RB5 = 21,
+    GPIO_PIN_RB6 = 22,
+    GPIO_PIN_RB7 = 23,
+    GPIO_PIN_RB8 = 24,
+    GPIO_PIN_RB9 = 25,
+    GPIO_PIN_RB10 = 26,
+    GPIO_PIN_RB11 = 27,
+    GPIO_PIN_RB12 = 28,
+    GPIO_PIN_RB13 = 29,
+
+    /* This element should not be used in any of the GPIO APIs.
+       It will be used by other modules or application to denote that none of the GPIO Pin is used */
+    GPIO_PIN_NONE = -1
+
+} GPIO_PIN;
+
+
+void GPIO_Initialize(void);
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: GPIO Functions which operates on multiple pins of a port
+// *****************************************************************************
+// *****************************************************************************
+
+uint32_t GPIO_PortRead(GPIO_PORT port);
+
+void GPIO_PortWrite(GPIO_PORT port, uint32_t mask, uint32_t value);
+
+uint32_t GPIO_PortLatchRead ( GPIO_PORT port );
+
+void GPIO_PortSet(GPIO_PORT port, uint32_t mask);
+
+void GPIO_PortClear(GPIO_PORT port, uint32_t mask);
+
+void GPIO_PortToggle(GPIO_PORT port, uint32_t mask);
+
+void GPIO_PortInputEnable(GPIO_PORT port, uint32_t mask);
+
+void GPIO_PortOutputEnable(GPIO_PORT port, uint32_t mask);
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: GPIO Functions which operates on one pin at a time
+// *****************************************************************************
+// *****************************************************************************
+
+static inline void GPIO_PinWrite(GPIO_PIN pin, bool value)
+{
+    GPIO_PortWrite((GPIO_PORT)(GPIOA_BASE_ADDRESS + (0x100 * (pin>>4))), (uint32_t)(0x1) << (pin & 0xF), (uint32_t)(value) << (pin & 0xF));
+}
+
+static inline bool GPIO_PinRead(GPIO_PIN pin)
+{
+    return (bool)(((GPIO_PortRead((GPIO_PORT)(GPIOA_BASE_ADDRESS + (0x100 * (pin>>4))))) >> (pin & 0xF)) & 0x1);
+}
+
+static inline bool GPIO_PinLatchRead(GPIO_PIN pin)
+{
+    return (bool)((GPIO_PortLatchRead((GPIO_PORT)(GPIOA_BASE_ADDRESS + (0x100 * (pin>>4)))) >> (pin & 0xF)) & 0x1);
+}
+
+static inline void GPIO_PinToggle(GPIO_PIN pin)
+{
+    GPIO_PortToggle((GPIO_PORT)(GPIOA_BASE_ADDRESS + (0x100 * (pin>>4))), 0x1 << (pin & 0xF));
+}
+
+static inline void GPIO_PinSet(GPIO_PIN pin)
+{
+    GPIO_PortSet((GPIO_PORT)(GPIOA_BASE_ADDRESS + (0x100 * (pin>>4))), 0x1 << (pin & 0xF));
+}
+
+static inline void GPIO_PinClear(GPIO_PIN pin)
+{
+    GPIO_PortClear((GPIO_PORT)(GPIOA_BASE_ADDRESS + (0x100 * (pin>>4))), 0x1 << (pin & 0xF));
+}
+
+static inline void GPIO_PinInputEnable(GPIO_PIN pin)
+{
+    GPIO_PortInputEnable((GPIO_PORT)(GPIOA_BASE_ADDRESS + (0x100 * (pin>>4))), 0x1 << (pin & 0xF));
+}
+
+static inline void GPIO_PinOutputEnable(GPIO_PIN pin)
+{
+    GPIO_PortOutputEnable((GPIO_PORT)(GPIOA_BASE_ADDRESS + (0x100 * (pin>>4))), 0x1 << (pin & 0xF));
+}
+
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+    }
+
+#endif
+// DOM-IGNORE-END
+#endif // PLIB_GPIO_H
