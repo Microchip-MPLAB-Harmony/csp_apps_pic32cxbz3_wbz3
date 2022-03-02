3040eab901bc9f23d5150e5f54447530a9d3b8e6
Arpan Kumar <arpan.kumar@microchip.com>
2022-03-02 11:34:53 +0530

    [ADCHS] added polled demo
    MH3-63247

---
+++ apps/adchs/adchs_polled/firmware/src/packs/WBZ351_DFP/instance/sercom2.h
@@ -0,0 +1,73 @@
+/*
+ * Instance header file for WBZ351
+ *
+ * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
+ *
+ * Subject to your compliance with these terms, you may use Microchip software and any derivatives
+ * exclusively with Microchip products. It is your responsibility to comply with third party license
+ * terms applicable to your use of third party software (including open source software) that may
+ * accompany Microchip software.
+ *
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY,
+ * APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL
+ * LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF
+ * MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE FULLEST EXTENT
+ * ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT
+ * EXCEED THE AMOUNT OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ */
+
+/* file generated from device description version 2022-02-03T13:54:44Z */
+#ifndef _WBZ35_SERCOM2_INSTANCE_
+#define _WBZ35_SERCOM2_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for SERCOM2 peripheral ========== */
+#define SERCOM2_SPI                              _UL_(0)    
+#define SERCOM2_USART                            _UL_(0)    
+#define SERCOM2_INSTANCE_ID                      _UL_(67)   
+#define SERCOM2_CHIP_SERCOM_PADDR_MSB            _UL_(31)   
+#define SERCOM2_CHIP_SERCOM_CLK_REDUCTION        _UL_(1)    
+#define SERCOM2_CHIP_SERCOM_DMA                  _UL_(1)    
+#define SERCOM2_CHIP_SERCOM_FIFO_DEPTH_POWER     _UL_(1)    
+#define SERCOM2_CHIP_SERCOM_RETENTION_SUPPORT    _UL_(0)    
+#define SERCOM2_CHIP_SERCOM_SPI                  _UL_(0)    
+#define SERCOM2_CHIP_SERCOM_SPI_HW_SS_CTRL       _UL_(0)    
+#define SERCOM2_CHIP_SERCOM_SPI_ICSPACE_EXT      _UL_(0)    
+#define SERCOM2_CHIP_SERCOM_SPI_OZMO             _UL_(0)    
+#define SERCOM2_CHIP_SERCOM_SPI_WAKE_ON_SSL      _UL_(0)    
+#define SERCOM2_CHIP_SERCOM_TTBIT_EXTENSION      _UL_(1)    
+#define SERCOM2_CHIP_SERCOM_TWIM                 _UL_(1)    
+#define SERCOM2_CHIP_SERCOM_TWIS                 _UL_(1)    
+#define SERCOM2_CHIP_SERCOM_TWIS_AUTO_ACK        _UL_(1)    
+#define SERCOM2_CHIP_SERCOM_TWIS_GROUP_CMD       _UL_(1)    
+#define SERCOM2_CHIP_SERCOM_TWIS_SDASETUP_CNT_SIZE _UL_(8)    
+#define SERCOM2_CHIP_SERCOM_TWIS_SDASETUP_SIZE   _UL_(4)    
+#define SERCOM2_CHIP_SERCOM_TWIS_SUDAT           _UL_(1)    
+#define SERCOM2_CHIP_SERCOM_TWI_FASTMP           _UL_(1)    
+#define SERCOM2_CHIP_SERCOM_TWI_HSMODE           _UL_(1)    
+#define SERCOM2_CHIP_SERCOM_TWI_SCLSM_MODE       _UL_(1)    
+#define SERCOM2_CHIP_SERCOM_TWI_SMB_TIMEOUTS     _UL_(1)    
+#define SERCOM2_CHIP_SERCOM_TWI_TENBIT_ADR       _UL_(1)    
+#define SERCOM2_CHIP_SERCOM_USART                _UL_(0)    
+#define SERCOM2_CHIP_SERCOM_USART_AUTOBAUD       _UL_(0)    
+#define SERCOM2_CHIP_SERCOM_USART_COLDET         _UL_(0)    
+#define SERCOM2_CHIP_SERCOM_USART_FLOW_CTRL      _UL_(0)    
+#define SERCOM2_CHIP_SERCOM_USART_FRAC_BAUD      _UL_(0)    
+#define SERCOM2_CHIP_SERCOM_USART_IRDA           _UL_(0)    
+#define SERCOM2_CHIP_SERCOM_USART_ISO7816        _UL_(0)    
+#define SERCOM2_CHIP_SERCOM_USART_LIN_MASTER     _UL_(0)    
+#define SERCOM2_CHIP_SERCOM_USART_RS485          _UL_(0)    
+#define SERCOM2_CHIP_SERCOM_USART_SAMPA_EXT      _UL_(0)    
+#define SERCOM2_CHIP_SERCOM_USART_SAMPR_EXT      _UL_(0)    
+#define SERCOM2_CHIP_SERCOM_DLY_COMPENSATION     _UL_(1)    
+#define SERCOM2_CHIP_SERCOM_SE_CNT               _UL_(1)    
+#define SERCOM2_DMAC_ID_RX                       _UL_(8)    
+#define SERCOM2_DMAC_ID_TX                       _UL_(9)    
+#define SERCOM2_GCLK_ID_SLOW                     _UL_(4)    
+#define SERCOM2_GCLK_ID_CORE                     _UL_(4)    
+
+#endif /* _WBZ35_SERCOM2_INSTANCE_ */
