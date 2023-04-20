025d13b9a465de2a34b640cb4f628739aa2ec257
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-04-20 19:08:58 +0530

    [ADCHS] Regenerated demo for adchs_adchs_polled_wbz351_curiosity
    MH3-77655

--- apps/adchs/adchs_polled/firmware/src/packs/WBZ351_DFP/instance/tcc2.h
+++ apps/adchs/adchs_polled/firmware/src/packs/WBZ351_DFP/instance/tcc2.h
@@ -1,7 +1,7 @@
 /*
  * Instance header file for WBZ351
  *
- * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2023 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,25 +20,25 @@
  *
  */
 
-/* file generated from device description version 2022-04-19T15:30:24Z */
+/* file generated from device description version 2023-04-11T16:47:17Z */
 #ifndef _WBZ35_TCC2_INSTANCE_
 #define _WBZ35_TCC2_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for TCC2 peripheral ========== */
-#define TCC2_INSTANCE_ID                         (15)       
-#define TCC2_SIZE                                (16)       
-#define TCC2_MASTER_SLAVE_MODE                   (0)        
 #define TCC2_CC_NUM                              (2)        
-#define TCC2_OW_NUM                              (2)        
 #define TCC2_DITHERING                           (1)        
-#define TCC2_OTMX                                (1)        
-#define TCC2_DTI                                 (1)        
-#define TCC2_SWAP                                (1)        
-#define TCC2_PG                                  (1)        
-#define TCC2_DMAC_ID_OVF                         (24)       
 #define TCC2_DMAC_ID_MC0                         (25)       
 #define TCC2_DMAC_ID_MC1                         (26)       
+#define TCC2_DMAC_ID_OVF                         (24)       
+#define TCC2_DTI                                 (1)        
 #define TCC2_GCLK_ID                             (5)        
+#define TCC2_INSTANCE_ID                         (15)       
+#define TCC2_MASTER_SLAVE_MODE                   (0)        
+#define TCC2_OTMX                                (1)        
+#define TCC2_OW_NUM                              (2)        
+#define TCC2_PG                                  (1)        
+#define TCC2_SIZE                                (16)       
+#define TCC2_SWAP                                (1)        
 
 #endif /* _WBZ35_TCC2_INSTANCE_ */
