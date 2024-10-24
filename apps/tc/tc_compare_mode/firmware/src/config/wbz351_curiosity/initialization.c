24c92235559a3500b01485ef8a9642dbd23fd445
Pranjal Das <pranjal.das@microchip.com>
2024-10-24 12:38:02 +0530

    [TC] Regenerated demo for tc__tc_compare_mode__firmware__wbz351_curiosity.X
    MH3-100216

--- apps/tc/tc_compare_mode/firmware/src/config/wbz351_curiosity/initialization.c
+++ apps/tc/tc_compare_mode/firmware/src/config/wbz351_curiosity/initialization.c
@@ -47,7 +47,6 @@
 #include "device.h"
 
 
-
 // ****************************************************************************
 // ****************************************************************************
 // Section: Configuration Bits
@@ -76,6 +75,7 @@
 #pragma config INT0P =      NEG
 #pragma config FECCCON =         OFF
 #pragma config FRECCDIS =      OFF
+
 
 
 /*** DEVCFG1 ***/
@@ -140,7 +140,7 @@
 
 /*** FBCFG0 ***/
 #pragma config BINFOVALID =      VALID
-#pragma config PCSCMODE =      SINGLE
+#pragma config PCSCMODE =      DUAL
 
 /*** FCPN0 ***/
 #pragma config CP =      DISABLED
@@ -154,6 +154,12 @@
 // Section: Driver Initialization Data
 // *****************************************************************************
 // *****************************************************************************
+/* Following MISRA-C rules are deviated in the below code block */
+/* MISRA C-2012 Rule 7.2 - Deviation record ID - H3_MISRAC_2012_R_7_2_DR_1 */
+/* MISRA C-2012 Rule 11.1 - Deviation record ID - H3_MISRAC_2012_R_11_1_DR_1 */
+/* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+/* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
+
 
 
 // *****************************************************************************
@@ -183,7 +189,7 @@
 // *****************************************************************************
 // *****************************************************************************
 
-
+/* MISRAC 2012 deviation block end */
 
 /*******************************************************************************
   Function:
@@ -197,18 +203,19 @@
 
 void SYS_Initialize ( void* data )
 {
+
     /* MISRAC 2012 deviation block start */
     /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
 
   
-    CLK_Initialize();
+    CLOCK_Initialize();
     /* MISRAC 2012 deviation block start */
     /* MISRA C-2012 Rule 11.1 deviated 1 time. Deviation record ID -  H3_MISRAC_2012_R_11_1_DR_1 */
 
     /* Configure Prefetch, Wait States by calling the ROM function whose address is available at address 0xF2D0 */
-    typedef int (*FUNC_PCHE_SETUP)(uint32_t setup);
+    typedef void (*FUNC_PCHE_SETUP)(uint32_t setup);
     (void)((FUNC_PCHE_SETUP)(*(uint32_t*)0xF2D0))((PCHE_REGS->PCHE_CHECON & (~(PCHE_CHECON_PFMWS_Msk | PCHE_CHECON_ADRWS_Msk | PCHE_CHECON_PREFEN_Msk)))
-                                    | (PCHE_CHECON_PFMWS(2) | PCHE_CHECON_PREFEN(1)));
+                                    | (PCHE_CHECON_PFMWS(2) | PCHE_CHECON_PREFEN(1)  | PCHE_CHECON_ADRWS(0)));
 
     /* MISRAC 2012 deviation block end */
 
@@ -225,14 +232,11 @@ void SYS_Initialize ( void* data )
     TC2_CompareInitialize();
 
 
-
-
-
     NVIC_Initialize();
+
 
     /* MISRAC 2012 deviation block end */
 }
-
 
 /*******************************************************************************
  End of File
